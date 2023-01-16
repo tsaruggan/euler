b6a1ca720fad9b0539dc6d5e742643cb81bd8580
tsaruggan <tsaruggan@gmail.com>
2023-01-16 12:59:14 -0500

    Initial Commit

---
+++ textmathapp/textmathappApp.swift
@@ -0,0 +1,17 @@
+//
+//  textmathappApp.swift
+//  textmathapp
+//
+//  Created by Saruggan Thiruchelvan on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct textmathappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
