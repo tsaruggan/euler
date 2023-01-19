df32d4fd4cbde70e67f24f3b83dd2ee19f3ba950
tsaruggan <tsaruggan@gmail.com>
2023-01-18 21:27:32 -0500

    custom symbols in TMKeyboardAppearance

--- textmathkeyboard/TMKeyboardAppearance.swift
+++ textmathkeyboard/TMKeyboardAppearance.swift
@@ -5,4 +5,26 @@
 //  Created by Saruggan Thiruchelvan on 2023-01-18.
 //
 
-import Foundation
+import KeyboardKit
+import SwiftUI
+
+class TMKeyboardAppearance: StandardKeyboardAppearance {
+    override func buttonImage(for action: KeyboardAction) -> Image? {
+        if case .keyboardType(let keyboardType) = action {
+            switch keyboardType {
+            case .numeric:
+                return Image(systemName: "textformat.123")
+            case .symbolic:
+                return Image(systemName: "rectangle.3.group.bubble.left")
+            case .emojis:
+                return Image(systemName: "atom")
+            default:
+                return action.standardButtonImage(for: keyboardContext)
+            }
+            
+        } else {
+            return super.buttonImage(for: action)
+        }
+    }
+    
+}
