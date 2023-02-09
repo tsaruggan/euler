11aae2e1962470e998aac4ef902115756c35986b
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2023-02-09 11:27:39 -0500

    new button images

--- textmathkeyboard/TMKeyboardAppearance.swift
+++ textmathkeyboard/TMKeyboardAppearance.swift
@@ -12,12 +12,10 @@ class TMKeyboardAppearance: StandardKeyboardAppearance {
     override func buttonImage(for action: KeyboardAction) -> Image? {
         if case .keyboardType(let keyboardType) = action {
             switch keyboardType {
-            case .numeric:
-                return Image(systemName: "textformat.123")
-            case .symbolic:
-                return Image(systemName: "rectangle.3.group.bubble.left")
             case .emojis:
-                return Image(systemName: "atom")
+                return Image(uiImage: "π".textToImage(fontSize: 18.5)!)
+            case .symbolic:
+                return Image(uiImage: "#$%".textToImage(fontSize: 14)!)
             default:
                 return action.standardButtonImage(for: keyboardContext)
             }
@@ -27,4 +25,23 @@ class TMKeyboardAppearance: StandardKeyboardAppearance {
         }
     }
     
+}
+
+import UIKit
+extension String {
+    func textToImage(fontSize: CGFloat = 17) -> UIImage? {
+        let nsString = (self as NSString)
+        let font = UIFont.systemFont(ofSize: fontSize) // you can change your font size here
+        let stringAttributes = [NSAttributedString.Key.font: font]
+        let imageSize = nsString.size(withAttributes: stringAttributes)
+
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0) //  begin image context
+        UIColor.clear.set() // clear background
+        UIRectFill(CGRect(origin: CGPoint(), size: imageSize)) // set rect size
+        nsString.draw(at: CGPoint.zero, withAttributes: stringAttributes) // draw text within rect
+        let image = UIGraphicsGetImageFromCurrentImageContext() // create image from context
+        UIGraphicsEndImageContext() //  end image context
+
+        return image ?? UIImage()
+    }
 }
