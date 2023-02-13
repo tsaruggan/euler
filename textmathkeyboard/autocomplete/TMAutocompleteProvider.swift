5976b9343307b96639710911fffb113bcdc20a38
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2023-02-12 22:12:25 -0500

    autocomplete galore

--- textmathkeyboard/autocomplete/TMAutocompleteProvider.swift
+++ textmathkeyboard/autocomplete/TMAutocompleteProvider.swift
@@ -14,18 +14,13 @@ class TMAutocompleteProvider: AutocompleteProvider {
     
     func autocompleteSuggestions(for text: String, completion: AutocompleteCompletion) {
         let lastWord = text.components(separatedBy: " ").last ?? ""
-        //        if text.isEmpty {
-        //            return completion(.success([]))
-        //        } else {
-        //            completion(.success(suggestions(for: text)))
-        //        }
-        
-        if lastWord.contains("^") {
+        if text.isEmpty {
+            return completion(.success([]))
+        } else if lastWord.contains("^") {
             return completion(.success(superscriptSuggestion(for: text)))
         } else if lastWord.contains("_") {
             return completion(.success(subscriptSuggestion(for: text)))
         }
-        return completion(.success([]))
     }
     
     var canIgnoreWords: Bool { false }
@@ -43,15 +38,7 @@ class TMAutocompleteProvider: AutocompleteProvider {
 
 private extension TMAutocompleteProvider {
     
-    func suggestions(for text: String) -> [AutocompleteSuggestion] {
-        return [
-            suggestion(text + "ly"),
-            suggestion(text + "er", "Subtitle"),
-            suggestion(text + "ter")
-        ]
-    }
-    
-    func suggestion(_ word: String, _ subtitle: String? = nil) -> AutocompleteSuggestion {
+    func autocompleteSuggestion(_ word: String, _ subtitle: String? = nil) -> AutocompleteSuggestion {
         StandardAutocompleteSuggestion(
             text: word,
             title: word,
@@ -60,24 +47,14 @@ private extension TMAutocompleteProvider {
     
     func superscriptSuggestion(for text: String) -> [AutocompleteSuggestion] {
         let componentsArr = text.split(separator: "^", maxSplits: 1)
-        let base: String = String(componentsArr[0])
+        let base: String = componentsArr.count > 0 ? String(componentsArr[0]) : ""
         let superscripts: String = componentsArr.count > 1 ? String(componentsArr[1]) : ""
         
-        let superscriptsDict = [
-            "1": "¹",
-            "2": "²",
-            "3": "³",
-            "4": "⁴",
-            "5": "⁵",
-            "6": "⁶",
-            "7": "⁷",
-            "8": "⁸",
-            "9": "⁹",
-            "0": "⁰"
-        ]
+        if superscripts.isEmpty { return [] }
+        
         var superscriptFull = ""
         for candidateSuperscript in superscripts {
-            if let sup = superscriptsDict[String(candidateSuperscript)] {
+            if let sup = Input.superscripts[String(candidateSuperscript)] {
                 superscriptFull += sup
             } else {
                 return []
@@ -85,30 +62,19 @@ private extension TMAutocompleteProvider {
         }
         
         let suggestion: String = base + superscriptFull
-        return [StandardAutocompleteSuggestion(text: suggestion, title: suggestion)]
+        return [autocompleteSuggestion(suggestion)]
     }
     
     func subscriptSuggestion(for text: String) -> [AutocompleteSuggestion] {
         let componentsArr = text.split(separator: "_", maxSplits: 1)
-        let base: String = String(componentsArr[0])
+        let base: String = componentsArr.count > 0 ? String(componentsArr[0]) : ""
         let subscripts: String = componentsArr.count > 1 ? String(componentsArr[1]) : ""
         
-        let subscriptsDict = [
-            "1": "₁",
-            "2": "₂",
-            "3": "₃",
-            "4": "₄",
-            "5": "₅",
-            "6": "₆",
-            "7": "₇",
-            "8": "₈",
-            "9": "₉",
-            "0": "₀",
-        ]
+        if subscripts.isEmpty { return [] }
         
         var subscriptFull = ""
         for candidateSubscript in subscripts {
-            if let sub = subscriptsDict[String(candidateSubscript)] {
+            if let sub = Input.subscripts[String(candidateSubscript)] {
                 subscriptFull += sub
             } else {
                 return []
@@ -116,6 +82,6 @@ private extension TMAutocompleteProvider {
         }
         
         let suggestion: String = base + subscriptFull
-        return [StandardAutocompleteSuggestion(text: suggestion, title: suggestion)]
+        return [autocompleteSuggestion(suggestion)]
     }
 }
