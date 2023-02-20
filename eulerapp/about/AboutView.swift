466e6d6c9d9a8c05de9fbbcddc7babc11510cb5d
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2023-02-20 12:35:24 -0500

    about page

--- eulerapp/about/AboutView.swift
+++ eulerapp/about/AboutView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import KeyboardKit
 
 struct AboutView: View {
     var body: some View {
-        Text("About")
+        VStack(spacing: 24) {
+            Text("euler is an open source ios keyboard for texting mathematical symbols and formulae developed by @tsaruggan. to enable the keyboard, open the app's settings and allow access to keyboards.")
+                .font(.system(size: 18))
+           
+            HStack(spacing: 48) {
+                Button {
+                    let url = URL(string: UIApplication.openSettingsURLString)!
+                    UIApplication.shared.open(url)
+                } label: {
+                    Label("settings",systemImage: "gearshape")
+                }
+                
+                Button {
+                    let url = URL(string: "https://github.com/tsaruggan/euler")!
+                    UIApplication.shared.open(url)
+                } label: {
+                    Label("github", systemImage: "internaldrive")
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .multilineTextAlignment(.leading)
+
     }
+    
 }
 
 struct AboutView_Previews: PreviewProvider {
